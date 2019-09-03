79d1eab8c7e87237096c7b4e9f433085ee56c77a
zie <zie@qq.com>
2019-09-04 00:59:31 +0800

    9-04 1:00

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,21 +1,10 @@
 import React, { Component } from 'react'
 import './Login.less';
-import easyJsonp from 'easy-jsonp';
-
-import axios from 'axios';
-const url = 'https://acg.toubiec.cn/random?return=json';
+const url = './img/6417838876e128b76ad6cab90ada45a8.jpg';
 export default class Login extends Component {
-    componentDidMount = () => {
-        easyJsonp({
-            url,
-            callback: 'callback',
-            timeout: 3000
-        }).then((data) => {
-            console.log(data);
-            
-        }).cacth((err) => {
-            console.log(err);
-        })
+    
+    onLoadHandle = (e) => {
+        e.target.style.opacity = 1;
     }
 
     render() {
@@ -24,7 +13,9 @@ export default class Login extends Component {
                 <div className="login-header">
                     <h1>React项目：一个用react编制的后台管理系统</h1>
                 </div>
-                <div className="login-content"></div>
+                <div className="login-content">
+                    <img src={url} alt="bg" onLoad={this.onLoadHandle}/>
+                </div>
             </div>
         )
     }
