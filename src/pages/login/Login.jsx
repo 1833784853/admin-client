25a9f91d671a784a671bcdf71dc1b330ae8161c8
zie <zie@qq.com>
2019-09-04 22:15:35 +0800

    9-04 20:15

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,25 +1,64 @@
 import React, { Component } from 'react'
+import { Form, Icon, Input, Button } from 'antd';
+
 import './Login.less';
 import url from './img/bgc.jpg';
-export default class Login extends Component {
-    
+class Login extends Component {
+
     onLoadHandle = (e) => {
         e.target.style.opacity = 1;
     }
 
+    handleSubmit = e => {
+        e.preventDefault();
+        alert('nihao');
+    };
+
     render() {
+        let { getFieldDecorator } = this.props.form;
         return (
             <div id="login-wrap">
                 <div className="login-header">
                     <h1>React项目：一个用react编制的后台管理系统</h1>
                 </div>
                 <div id="login-content">
-                    <img src={url} alt="bg" onLoad={this.onLoadHandle}/>
+                    <img src={url} alt="bg" onLoad={this.onLoadHandle} />
                     <div className="form-box">
                         <h1>进入新世界</h1>
+                        <Form onSubmit={this.handleSubmit} className="login-form">
+                            <Form.Item>
+                                {
+                                    getFieldDecorator('userName', {
+                                        rules:[
+                                            {required: true,whitespace: true, message:'用户名不能为空'},
+                                            {min: 4 ,message:'用户名不能少于4位'},
+                                            {max: 4,message:'用户名不能大于12位'},
+                                            {pattern: /^[0-9A-Za-z_]+$/ ,message:'用户名必须是英文或数字或下划线'}
+                                        ],
+                                    })(
+                                        <Input
+                                            prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                            placeholder="Username"
+                                        />)
+                                }
+                            </Form.Item>
+                            <Form.Item>
+                                <Input
+                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                                    type="password"
+                                    placeholder="Password"
+                                />
+                            </Form.Item>
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" className="login-form-button">进入
+                                </Button>
+                            </Form.Item>
+                        </Form>
                     </div>
                 </div>
             </div>
         )
     }
 }
+let WrappedLoginForm = Form.create()(Login);
+export default WrappedLoginForm;
